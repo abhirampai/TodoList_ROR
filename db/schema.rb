ecc5f05e81a41294db4d515e4063a0dd83b327f3
Pai026 <abhirampai1999@gmail.com>
2021-08-09 12:43:28 +0530

    Added Task model

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_063335) do
+ActiveRecord::Schema.define(version: 2021_08_09_071216) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "body"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
