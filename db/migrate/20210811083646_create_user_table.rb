04cdecce57e401826e03e0173f23817a915f2424
Pai026 <abhirampai1999@gmail.com>
2021-08-12 11:55:00 +0530

    Added rake tasks

--- db/migrate/20210811083646_create_user_table.rb
+++ db/migrate/20210811083646_create_user_table.rb
@@ -2,7 +2,7 @@
 
 class CreateUserTable < ActiveRecord::Migration[6.1]
   def change
-    create_table :tasks do |t|
+    create_table :users do |t|
       t.string :name, null: false
 
       t.timestamps
