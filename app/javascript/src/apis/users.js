b1423a0a75bda581f693138f437f27dcfdcaf456
Pai026 <abhirampai1999@gmail.com>
2021-08-12 12:19:35 +0530

    Added ability to logout

--- app/javascript/src/apis/users.js
+++ app/javascript/src/apis/users.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 const login = payload => axios.post("/sessions", payload);
 
+const signout = () => axios.delete("/sessions");
+
 const signup = payload => axios.post("/users", payload);
 
 const authApi = {
   login,
+  signout,
   signup
 };
 
