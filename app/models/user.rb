8fa9e210eaced6f32a62685db0c6d0eb3168de2a
Pai026 <abhirampai1999@gmail.com>
2021-09-30 11:46:15 +0530

    added feature when user is deleted his tasks would be deleted and assigned task gets assigned to owner of the task

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
 
-  has_many :tasks, dependent: :destroy, foreign_key: :user_id
+  has_many :tasks, foreign_key: :user_id
   has_many :user_notifications, dependent: :destroy, foreign_key: :user_id
   has_one :preference, dependent: :destroy, foreign_key: :user_id
   has_many :comments, dependent: :destroy
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   before_save :to_lowercase
   before_create :build_default_preference
+  before_destroy :change_task_owner_or_delete
 
   private
 
@@ -28,6 +29,19 @@ class User < ApplicationRecord
 
     def build_default_preference
       self.build_preference(notification_delivery_hour: Constants::DEFAULT_NOTIFICATION_DELIVERY_HOUR)
+    end
+
+    def change_task_owner_or_delete
+      tasks_to_change_owner = Task.where(user_id: self.id)
+      tasks_to_change_owner.each do |task|
+        task.user_id = task.creator_id
+        task.save!
+      end
+      if self.tasks
+        self.tasks.each do |task|
+        task.destroy
+      end
+      end
     end
 end
 
